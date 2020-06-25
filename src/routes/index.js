336b70f4189bfa9c334169786c9dd44f09359cdc
aks010 <code.aks.010@gmail.com>
2020-06-25 23:54:35 +0530

    Removed old structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use("/banners", require("./banners"));
-router.use("/microservices", require("./microservice"));
-router.use("/counters", require("./counter"));
-router.use("/buyselltools", require("./buySellTool"));
-router.use("/browsecategory", require("./browseCategory"));
-router.use("/browsecategoryitem", require("./browseCategoryItem"));
 router.use("/articles", require("./articles"));
 router.use("/custom", require("./custom"));
 router.use("/category", require("./category"));
